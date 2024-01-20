db069712319daddd618b71c215f4d758179454e9
Alexander Pacheco <alexanderjpacheco19@gmail.com>
2024-01-20 12:13:34 -0500

    Initial HomeScreen complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from 'react-router-dom';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+
 
 
 const App = () => {
@@ -9,7 +11,7 @@ const App = () => {
       <Header />
       <main className = "py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
