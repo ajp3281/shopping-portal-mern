dae3758ccb2a7a1d90f33dfd677e0a7fa464942b
Alexander Pacheco <alexanderjpacheco19@gmail.com>
2024-02-03 14:16:30 -0500

    Finished cart page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice.js';
+import cartSliceReducer from './slices/cartSlice.js';
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSliceReducer,
     },
     middleware: (getDefaultMidleware) => getDefaultMidleware().concat(apiSlice.middleware),
     devtools: true,
