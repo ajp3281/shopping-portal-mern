9ec8f3129551d63470b94e211148e125fdcd275a
Alexander Pacheco <alexanderjpacheco19@gmail.com>
2024-02-27 17:41:46 -0500

    Finished Production setup

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 // cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
-
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -39,9 +33,25 @@ app.get('/api/config/paypal', (req, res) =>
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req,res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html') )
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running');
+    });
+}
+
 app.use(notFound);
 
 app.use(errorHandler);
 
 
-app.listen(port, () => console.log(`Server running on port ${port}`)) 
+app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)) 
