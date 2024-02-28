f2fef3479458a08e573214692185dd9c8fa2b715
Alexander Pacheco <alexanderjpacheco19@gmail.com>
2024-02-27 19:39:47 -0500

    Bug Fixes Completed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { protect, admin } from '../middleware/authMiddleware.js';
+import checkObjectId from '../middleware/checkObjectId.js';
 const router = express.Router();
 import { getProducts, getProductById, createProduct, deleteProduct, updateProduct, createProductReview, getTopProducts } from '../controllers/productController.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.get('/top', getTopProducts);
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
-router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/:id').get(checkObjectId, getProductById).put(protect, admin, checkObjectId, updateProduct).delete(protect, admin, checkObjectId, deleteProduct);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 
 
 
